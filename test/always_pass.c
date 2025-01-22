0a541787a8e2e7160e37573f564733ee69146fa6
Cole Rottenberg <cole.rottenberg@honeywell.com>
2025-01-22 13:57:39 -0500

    adding a failed test

--- test/always_pass.c
+++ test/always_pass.c
@@ -7,9 +7,11 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_always_pass(void) { TEST_ASSERT_TRUE(1); }
+void test_always_fail(void) { TEST_ASSERT_TRUE(0); }
 
 int main() {
   UNITY_BEGIN();
   RUN_TEST(test_always_pass);
+  RUN_TEST(test_always_fail);
   return UNITY_END();
 }

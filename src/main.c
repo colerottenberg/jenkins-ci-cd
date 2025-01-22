d009bcb3cdaffd576ddd80500eaef13d1facbf48
Cole Rottenberg <cole.rottenberg@honeywell.com>
2025-01-22 13:30:37 -0500

    basic c prog

--- src/main.c
+++ src/main.c
@@ -13,10 +13,15 @@ void point_init(Point *self, int x, int y, int z) {
   self->z = z;
 }
 
+/*
+ * # Point Deinit
+ *
+ * - Used to free the attached data
+ */
 void point_deinit(Point *self) { free(self); }
 
 int main() {
-  printf("Hello World");
+  printf("Hello World\n");
   Point **arr = (Point **)malloc(sizeof(Point *) * 4);
   static int count = 0;
   for (int i = 0; i < 4; i++) {
@@ -24,6 +29,10 @@ int main() {
     arr[i]->x = count++;
     arr[i]->y = count++;
     arr[i]->z = count++;
+    printf("|x:%d|y:%d|z:%d|\n", arr[i]->x, arr[i]->y, arr[i]->z);
+  }
+  for (size_t i = 0; i < 4; i++) {
+    point_deinit(arr[i]);
   }
   return EXIT_SUCCESS;
 }

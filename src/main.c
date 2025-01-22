dd0fdbcaa61250991251b8e05bcc322267fb8d82
Cole Rottenberg <cole.rottenberg@honeywell.com>
2025-01-22 12:46:55 -0500

    working on a c testing lib

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Point {
+  int x;
+  int y;
+  int z;
+} Point;
+
 int main(int argc, char *argv[]) {
   printf("Hello World");
+  Point **arr = (Point **)malloc(sizeof(Point *) * 4);
+  static int count = 0;
+  for (int i = 0; i < 4; i++) {
+    arr[i] = (Point *)malloc(sizeof(Point));
+    arr[i]->x = count++;
+    arr[i]->y = count++;
+    arr[i]->z = count++;
+  }
   return EXIT_SUCCESS;
 }
---
+++ tests/always_pass.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unity.h>
+
+int main(int argc, char *argv[]) {
+  TEST_ASSERT(true);
+  return EXIT_SUCCESS;
+}
